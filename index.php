3faf2762161bf74fce2164fc08d46598fdcd1b35
Albisyx <a.spadoni9@campus.uniurb.it>
2018-05-14 10:37:08 +0200

    aggiunta codice per creare un semplice bot telegram di test

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
-echo "ciao, MAleysia Airlines";
+$token = "571961944:AAEy4eD_Nbu2l0B-u65tqQKkS2puWTm-Sic";
+$website = "https://api.telegram.org/bot".$token;
+
+$content = file_get_contents('php://input');
+
+if(!$content)
+	exit();
+
+$update = json_decode($content, false); // mettendo false otterremo un oggetto php
+$message = $update->message;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$username = $message->from->username;
+
+error_log("message text {$text}: {$username}\n");
 
 ?>
